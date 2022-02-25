664f52e6fa10dc4808868ff461158cf80f092f78
Doodoo <doodoo1014@gmail.com>
2022-02-26 07:20:12 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path 
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,10 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name="post_draft_list"),
+    path('post/<pk>/puslish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
+    path('drafts/$', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/puslish/', views.post_publish, name='post_publish'),
 ]
